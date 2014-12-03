31b742e4bf2972b2de41010df9fd37062ff9cba4
John <smellsofbikes@users.noreply.github.com>
2014-12-02 23:49:57 -0700

    Update gcode_arc_generator.c
    version compiles correctly and functions.  I haven't checked the gcode for correctness yet.

--- gcode_arc_generator.c
+++ gcode_arc_generator.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <math.h>
 
 int main(int argc, char *argv[])
@@ -28,7 +27,7 @@ int main(int argc, char *argv[])
 
   printf("Calculating cut radius %f using cutter diameter %f\n", arc_radius, cutter_diameter);
   offset = arc_radius + (cutter_diameter/2);
-  
+
   if ((file = fopen("output.ngc", "w")) == NULL)
   {
     printf("Failed to open output file, dumping output to screen\n");
@@ -42,6 +41,7 @@ int main(int argc, char *argv[])
     fprintf(file, "G03 X%f Y%f I%f J%f F100\n", offset, 0.0, 0.0, offset);
     fprintf(file, "G03 X%f Y%f I%f J%f\n", 0.0, offset, offset, 0.0);
     fclose(file);
+    printf("Code written to 'output.ngc'\n");
   }
   return 0;
 }
